a5a2e44ec4a7cd8ea3d857cb266373b0a8285234
Mario López Matamala <mariomatamala12@gmail.com>
2023-04-17 21:31:55 +0200

    los bat necesario para la ejecución

---
+++ Kamisado1.0/ejecutar_gui.bat
@@ -0,0 +1,2 @@
+@echo off
+java -cp .\bin;.\lib\kamisado-gui-lib-3.0.0.jar;.\lib juego.gui.Kamisado
---
+++ Kamisado1.0/ejecutar_textui.bat
@@ -0,0 +1,3 @@
+@echo off
+java -cp .\bin;.\lib\kamisado-gui-lib-3.0.0.jar;.\lib juego.textui.Kamisado 
+pause
